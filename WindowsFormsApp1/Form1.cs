e1b6c3c648c6219da8223f0040c0d0d140059c74
Joerg-Busse <bbsf2abu@web.de>
2020-05-07 10:03:45 +0200

    Test added

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -5,8 +5,12 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
 
 namespace WindowsFormsApp1
 {
@@ -15,11 +19,28 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+
+
+
+           
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            IWebDriver driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("https://www.google.de/");
+            Thread.Sleep(2000);
+
+            IWebElement ele = driver.FindElement(By.Name("q"));
+            ele.SendKeys("triebwerk duisburg");
+            Thread.Sleep(2000);
+
+            IWebElement eleBtn = driver.FindElement(By.Name("btnK"));
+            eleBtn.Click();
+            Thread.Sleep(2000);
+
+            driver.Close();
         }
     }
 }
