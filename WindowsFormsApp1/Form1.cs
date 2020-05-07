d8560d593739011b86021491d9958ca7b98944f3
Joerg-Busse <bbsf2abu@web.de>
2020-05-07 10:41:08 +0200

    Add javaScriptExecutor

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -11,33 +11,31 @@ using System.Windows.Forms;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 
-
 namespace WindowsFormsApp1
 {
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
-
-
-
-           
-
         }
 
         private void button1_Click(object sender, EventArgs e)
-        {
+        {           
+
             IWebDriver driver = new FirefoxDriver();
             driver.Navigate().GoToUrl("https://www.google.de/");
-            Thread.Sleep(2000);
+            Thread.Sleep(2000);            
 
             IWebElement ele = driver.FindElement(By.Name("q"));
             ele.SendKeys("triebwerk duisburg");
             Thread.Sleep(2000);
 
             IWebElement eleBtn = driver.FindElement(By.Name("btnK"));
-            eleBtn.Click();
+
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript("arguments[0].click();", eleBtn);           
             Thread.Sleep(2000);
 
             driver.Close();
